f37e064e4409147fac478bf5c0c42d96e6094457
Sergio Santos <ssantos@redhat.com>
2017-02-13 18:49:44 -0200

    JMS Agendada

--- test-fuse-architecture/src/test/java/com/redhat/brazil/consulting/fuse/messaging/jms/JmsClientTest.java
+++ test-fuse-architecture/src/test/java/com/redhat/brazil/consulting/fuse/messaging/jms/JmsClientTest.java
@@ -48,18 +48,30 @@ public class JmsClientTest {
 		System.out.println("Enviou");
 	}
 	
-	@Test
+	
 	public void testeMensagemAgendada() throws JMSException{
 		Queue queue = session.createQueue("testeMensagemAgendada");
 		MessageProducer producer = session.createProducer(queue);
 		
-		Message msg = session.createTextMessage("Teste Mensagem Agendada");
+		Message msg = session.createTextMessage("Teste Mensagem Agendada 9");
 		long time = 60 * 1000;
 		msg.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, time);
-		
+				
 		producer.send(msg);
 		
-		System.out.println("Enviou");
+		System.out.println("Enviou ");
+	}
+	
+	@Test
+	public void testeMensagemAgendadaConversao() throws JMSException{
+		Queue queue = session.createQueue("testeMensagemAgendadaConversaoIn");
+		MessageProducer producer = session.createProducer(queue);
+		
+		Message msg = session.createTextMessage("Teste Mensagem Agendada Conversao");
+				
+		producer.send(msg);
+		
+		System.out.println("Enviou ");
 	}
 	
 
